077118a9b0b0a602078456689a41fdd75f68a106
jeromehenry3 <jerome.henry3@gmail.com>
2019-04-15 13:07:10 +0200

    added function in repoContents for reseting scroll position when mounting

--- src/components/Search/RepoContents/index.js
+++ src/components/Search/RepoContents/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import {
   Table, Icon, Button, Label,
@@ -7,52 +7,60 @@ import {
 // importé depuis https://github.com/ozh/github-colors/blob/master/LICENSE
 import Colors from './colors.json';
 
-const RepoContents = ({ data, changeView }) => {
-  const toggleView = () => {
+class RepoContents extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0); // resets scroll position when mounting
+  }
+
+  toggleView = () => {
+    const { changeView } = this.props;
     changeView('search');
   };
 
-  return (
-    <Fragment>
-      <Button onClick={toggleView}>Retour</Button>
-      <Table celled striped>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell><span>Git Repository </span>
-              {
-                Object.keys(data.languages).map(language => (
-                  <Label
-                    key={language}
-                    style={{ borderBottom: `2px solid ${Colors[language].color}` }}
-                  >{language}
-                  </Label>
-                ))
-              }
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+  render() {
+    const { data } = this.props;
+    return (
+      <Fragment>
+        <Button onClick={this.toggleView}>Retour</Button>
+        <Table celled striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell><span>Git Repository </span>
+                {
+                  Object.keys(data.languages).map(language => (
+                    <Label
+                      key={language}
+                      style={{ borderBottom: `2px solid ${Colors[language].color}` }}
+                    >{language}
+                    </Label>
+                  ))
+                }
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-        <Table.Body>
+          <Table.Body>
 
-          {
-            data.filesList.map(({ type, name, sha }) => (
-              <Table.Row key={sha}>
-                <Table.Cell collapsing>
-                  <Icon name={type === 'file' ? 'file outline' : 'folder'} />
-                  {name}
-                </Table.Cell>
-              </Table.Row>
-            ))
-          }
-        </Table.Body>
-      </Table>
-    </Fragment>
-  );
-};
+            {
+              data.filesList.map(({ type, name, sha }) => (
+                <Table.Row key={sha}>
+                  <Table.Cell collapsing>
+                    <Icon name={type === 'file' ? 'file outline' : 'folder'} />
+                    {name}
+                  </Table.Cell>
+                </Table.Row>
+              ))
+            }
+          </Table.Body>
+        </Table>
+      </Fragment>
+    );
+  }
+}
 
 RepoContents.propTypes = {
   data: PropTypes.object.isRequired,
   changeView: PropTypes.func.isRequired,
-}
+};
 
 export default RepoContents;
